c12b7072f6635deb6afd22941c95e03fb217cbf6
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-13 14:25:36 -0700

    Added posts route to server

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { MONGO_URI } = require('./config')
+
+//ROUTES
+const postsRoute = require('./routes/api/posts')
+
 const app = express();
 
 //CONNET TO MONGODB
 mongoose.connect(MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => console.log('MongoDB connected!'))
   .catch(err => console.log(err));
+
+  //USER ROUTES
+  app.use('/api/posts', postsRoute);
+  
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server run at port ${PORT}`));
