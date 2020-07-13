02ef1174b121a5bd01d954f95fa3bfcf3de21d09
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-13 14:03:53 -0700

    config

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MONGO_URI: 'mongodb://rest_test:j4YZEBgrEQdy73cP@cluster0-shard-00-00.enjwl.mongodb.net:27017,cluster0-shard-00-01.enjwl.mongodb.net:27017,cluster0-shard-00-02.enjwl.mongodb.net:27017/stephanieblog?ssl=true&replicaSet=atlas-wx9tl2-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { MONGO_URI } = require('./config')
 const app = express();
 
-//ROUTES
-app.get('/', (req, res) => {
-  res.send('Hello from node');
-});
-
+//CONNET TO MONGODB
+mongoose.connect(MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log('MongoDB connected!'))
+  .catch(err => console.log(err));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server run at port ${PORT}`));
