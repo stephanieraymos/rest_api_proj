3195d1d063785a6edf106ceffe534f52aa95a8d1
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-13 14:23:33 -0700

    routes

---
+++ routes/api/posts.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.router();
+//posts model
+const Posts = require('../../models/Posts');
+
+//Routes POST api/posts
+//Description: create a post
+router.post('/', (req, res) => {
+  res.send(`Let's create post!`);
+})
+
+module.exports = router;
