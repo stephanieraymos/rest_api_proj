b5ba158df9b79ce7a1e5722c0632b093c28313bc
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-13 13:42:39 -0700

    Simple server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+//ROUTES
+app.get('/', (req, res)) => {
+  res.send('Hello from node');
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server run at port ${PORT}`));
